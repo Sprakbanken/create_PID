4bed11c55532312eaf67d6b6de0ddb7cdd8c4256
Magnus Breder Birkenes <12692786+magbb@users.noreply.github.com>
2025-11-19 12:48:33 +0100

    Gjør PID-verktøyet mer generisk (#1)
    * add HANDLE to .env
    
    * Make genereic script, overiding PIDs if needed.
    
    * Update README.md
    
    * Update .env-example
    
    * Update main.py
    
    * add list functionality; add argparser subparsers for commands
    
    * update README.md
    
    * Update README.md
    
    * fix get_pid()
    
    * Update main.py
    
    Co-authored-by: Tita <42641610+titaenstad@users.noreply.github.com>
    
    * exception handling of JSON deocding
    
    * print instead of yielding
    
    * Update main.py
    
    Co-authored-by: Tita <42641610+titaenstad@users.noreply.github.com>
    
    * if -> match
    
    * Update main.py
    
    Co-authored-by: Tita <42641610+titaenstad@users.noreply.github.com>
    
    * override -> update
    
    ---------
    
    Co-authored-by: Tita <42641610+titaenstad@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,24 +7,19 @@ import json
 import argparse
 from dotenv import load_dotenv
 
+load_dotenv()
 
-PID_HANDLE = "21.11146"
+HANDLE_PREFIX = os.environ["HANDLE_PREFIX"]
 
-
-def create_pid(sbr_num: int, username: str, password: str) -> None:
-    print(f"Creating PID for sbr-{sbr_num}")
-
-    sbr_id = f"sbr-{sbr_num}"
-    landingpage_url = (
-        f"https://www.nb.no/sprakbanken/ressurskatalog/oai-nb-no-{sbr_id}/"
-    )
+def create_pid(PID: str, url: str, username: str, password: str) -> None:
+    print(f"Creating PID for {PID}")
 
     headers = {"Content-Type": "application/json"}
 
-    url = f"https://pid.gwdg.de/handles/{PID_HANDLE}/{sbr_num}"
-    data = json.dumps([{"type": "URL", "parsed_data": landingpage_url}])
+    handle_url = f"https://pid.gwdg.de/handles/{HANDLE_PREFIX}/{PID}"
+    data = json.dumps([{"type": "URL", "parsed_data": url}])
     r = requests.put(
-        url=url,
+        url=handle_url,
         data=data,
         headers=headers,
         auth=HTTPBasicAuth(username, password),
@@ -34,47 +29,75 @@ def create_pid(sbr_num: int, username: str, password: str) -> None:
     print(f"PUT request status code: {r.status_code}")
 
 
-def pid_exists(sbr_num: int, username: str, password: str) -> bool:
+def get_pid(PID: str, username: str, password: str) -> dict | None:
     headers = {"Content-Type": "application/json"}
-    url = f"https://pid.gwdg.de/handles/{PID_HANDLE}/{sbr_num}"
+    handle_url = f"https://pid.gwdg.de/handles/{HANDLE_PREFIX}/{PID}"
 
     r = requests.get(
-        url=url,
+        url=handle_url,
         headers=headers,
         auth=HTTPBasicAuth(username=username, password=password),
     )
 
     if r.status_code == 404:
-        return False
+        return None
     if r.status_code == 200:
-        return True
+        try:
+            return r.json()
+        except requests.exceptions.JSONDecodeError:
+            print(f"JSONDecodeError when trying to parse content from request: {r}")
+            return None
     print(f"Unexpected status code: {r.status_code} for GET request to url: {r.url}")
-    return False
+    return None
 
+def pid_list(username: str, password: str) -> None:
+    headers = {"Content-Type": "application/json"}
+    handle_url = f"https://pid.gwdg.de/handles/{HANDLE_PREFIX}"
+
+    r = requests.get(
+        url=handle_url,
+        headers=headers,
+        auth=HTTPBasicAuth(username=username, password=password),
+    )
+
+    if r.status_code == 404:
+        print("Prefix not found.")
+    if r.status_code == 200:
+        pids = [pid for pid in r.content.decode("utf-8").split("\r\n") if pid.strip() != ""]
+        for pid in pids:
+            content = get_pid(pid, username, password)
+            if content:
+                print(pid, content[0]["type"], content[0]["parsed_data"])
+            else:
+                print(pid, None, None)
 
 if __name__ == "__main__":
-    load_dotenv()
-
     parser = argparse.ArgumentParser(
-        description="Create a PID for a Språkbanken resource"
+        description="Manage PIDs for language resources"
     )
-    parser.add_argument(
-        "sbr_num",
-        type=int,
-        help="Resource number (number after sbr- in the resource catalogue url)",
+
+    subparsers = parser.add_subparsers(dest="command", help="Subcommands")
+    parser_create = subparsers.add_parser("create", help="Create and modify PIDs.")
+    parser_list = subparsers.add_parser("list", help="List all PIDs.")
+
+    parser_create.add_argument(
+        "PID",
+        type=str,
+        help="The actual PID to create.",
     )
-    parser.add_argument(
-        "-c",
-        "--check_previous",
+
+    parser_create.add_argument(
+        "--url",
+        type=str,
+        help="The URL that the PID is associated with.",
+    )
+
+    parser_create.add_argument(
+        "--update",
+        help="Update an existing PID if it exists.",
         action="store_true",
-        help="If flagged, check if PID exist for all previous sbr-numbers (from 1 to sbr_num)",
     )
-    parser.add_argument(
-        "-a",
-        "--add_previous",
-        action="store_true",
-        help="If flagged, will create PIDs when they are missing for all previous sbr-numbers (from 1 to sbr_num)",
-    )
+
     args = parser.parse_args()
 
     for env_variable in ("username", "password"):
@@ -82,27 +105,22 @@ if __name__ == "__main__":
             print(f"environment variable '{env_variable}' is missing in .env file!")
             exit(1)
 
-    if args.check_previous:
-        print(f"Checking if PID exist for all sbr numbers upto {args.sbr_num}")
-        pid_missing_list = [
-            i
-            for i in range(1, args.sbr_num)
-            if not pid_exists(i, os.environ["username"], os.environ["password"])
-        ]
-        if pid_missing_list:
-            print(
-                f"PID missing for the following sbr numbers: {'\n'.join(str(i) for i in pid_missing_list)}"
-            )
-            if args.add_previous:
-                for i in pid_missing_list:
-                    create_pid(
-                        sbr_num=i,
-                        username=os.environ["username"],
-                        password=os.environ["password"],
-                    )
+    match args.command:
+        case "list":
+            pid_list(os.environ["username"], os.environ["password"])
+        case "create":
+            # first: check if PID exists
+            pid = get_pid(args.PID, os.environ["username"], os.environ["password"])
 
-        else:
-            print("No PIDs missing")
+            # if it exists, update only if update param is explicitly set, otherwise exit
+            if pid != None:
+                if args.update:
+                    print(f"{args.PID} already exists, updating as requested.")
+                    pass
+                else:
+                    print(f"{args.PID} already exists, leaving it untouched. Exiting.")
+                    exit(1)
 
-    create_pid(args.sbr_num, os.environ["username"], os.environ["password"])
-    print(f"PID link for COMEDI metadata:\nhdl:{PID_HANDLE}/{args.sbr_num}")
+            create_pid(args.PID, args.url, os.environ["username"], os.environ["password"])
+        case _:
+            parser.print_help()
